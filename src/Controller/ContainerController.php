1c1759c69311e71007048844d95ed0685594fe3c
Sara Carlstead Brumfield <saracarl@gmail.com>
2018-10-31 13:23:25 -0500

    Mirador integration with no customization

--- src/Controller/ContainerController.php
+++ src/Controller/ContainerController.php
@@ -30,32 +30,35 @@ class ContainerController extends AbstractActionController
             throw new NotFoundException;
         }
 
-        // // Map iiif resources with Omeka Classic and Omeka S records.
-        // $matchingResources = [
-        //     'item' => 'items',
-        //     'items' => 'items',
-        //     'item-set' => 'item_sets',
-        //     'item-sets' => 'item_sets',
-        //     'item_set' => 'item_sets',
-        //     'item_sets' => 'item_sets',
-        //     'collection' => 'item_sets',
-        //     'collections' => 'item_sets',
-        // ];
-        // $resourceName = $this->params('resourcename');
-        // if (!isset($matchingResources[$resourceName])) {
-        //     throw new NotFoundException;
-        // }
-        // $resourceName = $matchingResources[$resourceName];
+        // Map iiif resources with Omeka Classic and Omeka S records.
+        $matchingResources = [
+            'item' => 'items',
+            'items' => 'items',
+            'item-set' => 'item_sets',
+            'item-sets' => 'item_sets',
+            'item_set' => 'item_sets',
+            'item_sets' => 'item_sets',
+            'collection' => 'item_sets',
+            'collections' => 'item_sets',
+        ];
+        $resourceName = $this->params('resourcename');
+        if (!isset($matchingResources[$resourceName])) {
+            throw new NotFoundException;
+        }
+        $resourceName = $matchingResources[$resourceName];
 
-        // $response = $this->api()->read($resourceName, $id);
-        // $resource = $response->getContent();
-        // if (empty($resource)) {
-        //     throw new NotFoundException;
-        // }
+        $response = $this->api()->read($resourceName, $id);
+        $resource = $response->getContent();
+        if (empty($resource)) {
+            throw new NotFoundException;
+        }
 
         $view = new ViewModel;
         $view->setVariable('id', $id);
+        $view->setVariable('item', $resource);
+        $view->setVariable('resourceName', $resourceName);
 
+	    $view->setTerminal(true);
         return $view;
         // return $this->jsonLd("HELLO");
     }
